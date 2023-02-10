eea62f70b2ef0a05af7e317dea791cdec1bdb1db
SinisterGaze <edvkar98@gmail.com>
2023-02-11 00:04:32 +0100

    added ray-plane, ray-sphere intersection methods

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate impl_ops;
+use float_cmp::{self, approx_eq};
 use real_float;
 use std::ops;
 
@@ -40,10 +41,26 @@ impl_op_ex!(-|a: &Point3D| -> Point3D {
 });
 impl_op_ex!(+= |lhs: &mut Point3D, rhs: Point3D| {lhs.x += rhs.x; lhs.y += rhs.y; lhs.z += rhs.z;});
 impl_op_ex!(-= |lhs: &mut Point3D, rhs: Point3D| {lhs.x -= rhs.x; lhs.y -= rhs.y; lhs.z -= rhs.z;});
+impl_op_ex!(*= |lhs: &mut Point3D, rhs: &f64| {lhs.x *= rhs; lhs.y *= rhs; lhs.z *= rhs;});
+impl_op_ex!(/= |lhs: &mut Point3D, rhs: &f64| {lhs.x /= rhs; lhs.y /= rhs; lhs.z /= rhs;});
 
 impl Point3D {
     fn norm(&self) -> f64 {
         f64::sqrt(self * self)
+    }
+
+    fn norm2(&self) -> f64 {
+        self * self
+    }
+
+    fn normalize(&mut self) {
+        (*self) /= self.norm();
+    }
+
+    fn unit_vector(&self) -> Self {
+        let mut result = self.clone();
+        result.normalize();
+        result
     }
 }
 
@@ -73,16 +90,51 @@ pub struct Sphere {
     radius: f64,
 }
 
+// Solve for ray: p0 + t * p intersecting with sphere: |x-x0| = r
+// results in solving a quadratic formula at^2 + bt + c = 0 with
+// a = p^2
+// b = 2 * p * (p0 - c)
+// c = x0^2 + p0^2 - r^2 - 2 * p0 * c
+impl Object3D for Sphere {
+    fn intersect(&self, ray: Ray) -> Option<Point3D> {
+        let a = ray.direction.norm2(); // a = r^2
+        let b = 2.0 * (ray.direction * (ray.point - self.center));
+        let c = self.center.norm2() + ray.point.norm2()
+            - self.radius.powi(2)
+            - 2.0 * (ray.point * self.center);
+
+        let D = b * b - 4.0 * a * c;
+        if D < 0.0 || approx_eq!(f64, a, 0.0, ulps = 2) {
+            None
+        } else {
+            let t = (-b - D.sqrt())/(2.0*a);
+            Some(ray.point + t * ray.direction)
+        }
+    }
+}
+
 pub struct Plane {
     normal: Point3D,
     distance: f64,
+}
+
+impl Object3D for Plane {
+    fn intersect(&self, ray: Ray) -> Option<Point3D> {
+        if float_cmp::approx_eq!(f64, self.normal * ray.direction, 0.0, ulps = 2) {
+            None
+        } else {
+            let t: f64 = (self.distance * self.normal.norm() - ray.point * self.normal)
+                / (ray.direction * self.normal);
+            Some(ray.point + t * ray.direction)
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use real_float::IsFinite;
 
-    use crate::Point3D;
+    use crate::{Object3D, Plane, Point3D, Ray, Sphere};
 
     #[test]
     fn test_addition() {
@@ -105,7 +157,7 @@ mod tests {
             }
         );
     }
-    
+
     #[test]
     fn test_subtraction() {
         let a = Point3D {
@@ -187,5 +239,118 @@ mod tests {
         };
         assert_eq!(a * b, b * a);
         assert_eq!(a * b, 4.0);
+    }
+
+    #[test]
+    fn test_norm() {
+        let a = Point3D {
+            x: 1.0,
+            y: 2.0,
+            z: 2.0,
+        };
+        assert_eq!(a.norm(), 3.0); // sqrt(1*1 + 2*2 + 2*2) = sqrt(9) = 3
+        let b = a.unit_vector();
+        assert_eq!(b.norm(), 1.0); // norm of normalized vector is a unit vector
+        let zero = Point3D {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        assert_eq!(zero.norm(), 0.0); // norm of zero vector is zero
+    }
+
+    #[test]
+    fn test_plane_intersect() {
+        // Simple intersection of ray onto xy-plane
+        let my_plane = Plane {
+            normal: Point3D {
+                x: (0.0),
+                y: (0.0),
+                z: (1.0),
+            },
+            distance: 0.0,
+        };
+
+        let my_ray = Ray {
+            point: Point3D {
+                x: (1.0),
+                y: (2.0),
+                z: (-3.0),
+            },
+            direction: Point3D {
+                x: (0.0),
+                y: (0.0),
+                z: (1.0),
+            },
+        };
+
+        let intersection_point = my_plane.intersect(my_ray);
+        assert_eq!(
+            intersection_point.unwrap(),
+            Point3D {
+                x: 1.0,
+                y: 2.0,
+                z: 0.0
+            }
+        );
+
+        // Test of ray parallel to plane
+        // No (unique) intersection point
+        let parallel_ray = Ray {
+            point: Point3D {
+                x: (1.0),
+                y: (2.0),
+                z: (-3.0),
+            },
+            direction: Point3D {
+                x: (-5.0),
+                y: (7.0),
+                z: (0.0),
+            },
+        };
+        let intersection_point = my_plane.intersect(parallel_ray);
+        assert_eq!(intersection_point, None);
+
+        // Test compared to example gathered from https://www.kristakingmath.com/blog/intersection-of-a-line-and-a-plane
+        // Can't test for equality due to floating point precision, result is instead printed
+        let my_plane = Plane {
+            normal: Point3D {
+                x: (2.0),
+                y: (-3.0),
+                z: (1.0),
+            },
+            distance: 3.0 / f64::sqrt(14.0),
+        };
+
+        let my_ray = Ray {
+            point: Point3D {
+                x: (-1.0),
+                y: (4.0),
+                z: (1.0),
+            },
+            direction: Point3D {
+                x: (2.0),
+                y: (-5.0),
+                z: (1.0),
+            },
+        };
+        let intersection_point = my_plane.intersect(my_ray);
+        eprintln!("{}", intersection_point.unwrap()); // should be (0.6, 0, 1.8)
+    }
+
+    #[test]
+    fn test_sphere_intersect() {
+        let my_sphere = Sphere {
+            center: Point3D { x: (0.0), y: (0.0), z: (0.0) },
+            radius: 3.0,
+        };
+        let my_ray = Ray {
+            point: Point3D { x: (2.0), y: (2.0), z: (2.0) },
+            direction: Point3D { x: (-1.0), y: (-1.0), z: (-1.0) }
+        };
+        let intersection_point = my_sphere.intersect(my_ray).unwrap();
+        eprintln!("{}", intersection_point);
+        assert_eq!(intersection_point, Point3D{x: 0.0, y: 0.0, z: 3.0});
+
     }
 }
